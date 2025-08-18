bc70b71f78aeba15a86261c167c782f8a74ad15a
swdefrxgthvyjukidxfcvghjnbkmlo-code <swdefrxgthvyjukidxfcvghjnbkmlo@gmail.com>
2025-08-18 18:17:29 +0000

    noot

---
+++ main.py
@@ -0,0 +1,9 @@
+# Author: Ivan Chang
+# Date: August 18, 2025
+# Description: Prints statement about me
+
+# Print statements about me
+print("""Name: Ivan Chang
+Major: Electrical Engineering
+Hobbies: Programming, Piano, and Violin
+Reason for taking this class: This class is required for a degree in Cybersecurity""")
